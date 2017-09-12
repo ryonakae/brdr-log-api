7b106fed6cb55e82026285997b2aa15de9b37a67
Ryo Nakae <ryo.n.1129@gmail.com>
2017-09-12 17:41:35 +0900

    update webhook php

--- theme/public/github_webhook.php
+++ theme/public/github_webhook.php
@@ -1,10 +1,9 @@
 <?php
-  $SECRET_KEY = 'wR[a{[iU2;4bahA3YJ4dgPYCV6HGHc+aRE3jMfpFiw)K24[{%8{bNFTBzFLd3qyT';
-
-  if (isset($_GET['key']) && $_GET['key'] === $SECRET_KEY && isset($_POST['payload'])) {
+  if (isset($_POST['payload'])) {
     $payload = json_decode($_POST['payload'], true);
-    if ($payload['ref'] === 'refs/heads/master') {
-      `cd /home/ryo/service/brdr-log; git pull origin master; cd theme; npm i; npm run build`;
+    $branch = $payload['ref'];
+    if ($branch === 'refs/heads/master') {
+      exec('cd /home/ryo/service/brdr-log/theme; git pull origin master; npm install; npm run build');
     }
   } else {
     echo 'invalid access';
