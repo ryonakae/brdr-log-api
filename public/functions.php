1034e1ba4abe2f06a2e19c9e6db25efabfe5e436
Ryo Nakae <ryo.n.1129@gmail.com>
2017-11-17 03:51:17 +0900

    productionでだけcloudinary経由で画像を表示するように

--- public/functions.php
+++ public/functions.php
@@ -16,89 +16,80 @@ remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('admin_print_styles', 'print_emoji_styles');
 
 // head内のインラインスタイル削除
-function remove_recent_comments_style() {
+add_filter('widgets_init', function() {
   global $wp_widget_factory;
   remove_action('wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
-}
-add_action('widgets_init', 'remove_recent_comments_style');
+});
 
 // head内のdns-prefetchを削除
-function remove_dns_prefetch($hints, $relation_type) {
+add_filter('wp_resource_hints', function($hints, $relation_type) {
   if ('dns-prefetch' === $relation_type) {
     return array_diff(wp_dependencies_unique_hosts(), $hints);
   }
   return $hints;
-}
-add_filter('wp_resource_hints', 'remove_dns_prefetch', 10, 2);
+}, 10, 2);
 
 // アイキャッチ画像の有効化
 add_theme_support('post-thumbnails');
 
 // アイキャッチ画像生成時の画質を変更
-add_filter('jpeg_quality', function($arg){return 95;});
+add_filter('jpeg_quality', function($arg){ return 100; });
 
 // アイキャッチ画像のサイズを削除
-function remove_image_sizes($sizes) {
-	unset($sizes['thumbnail']);
-	// unset($sizes['medium']);
+add_filter('intermediate_image_sizes_advanced', function($sizes){
+  unset($sizes['thumbnail']);
+	unset($sizes['medium']);
 	unset($sizes['large']);
 	return $sizes;
-}
-add_filter('intermediate_image_sizes_advanced', 'remove_image_sizes');
+});
 // medium_large
 update_option('medium_large_size_w', 0);
 
 // アイキャッチ画像のサイズを追加
-// add_image_size('theme_dummy', 10, 10, false);
-add_image_size('theme_thumbnail', 2048, 2048, false);
+add_image_size('theme_thumbnail', 300, 300, true);
+add_image_size('theme_eyecatch', 2048, 2048, false);
+// add_image_size('theme_content', 1440, 1440, false);
 
 // セルフピンバックの無効化
-function no_self_ping(&$links){
+add_filter('pre_ping', function(&$links) {
   $home = get_option('home');
   foreach($links as $l => $link)
   if(0 === strpos($link, $home))
   unset($links[$l]);
-}
-add_action('pre_ping', 'no_self_ping');
+});
 
 // ビジュアルエディタを無効化
-function disable_visual_editor_mypost(){
-  add_filter('user_can_richedit', 'disable_visual_editor_filter');
+function disable_visual_editor(){
+  add_filter('user_can_richedit', function(){ return false; });
 }
-function disable_visual_editor_filter(){
-  return false;
-}
-add_action('load-post.php', 'disable_visual_editor_mypost');
-add_action('load-post-new.php', 'disable_visual_editor_mypost');
+add_filter('load-post.php', 'disable_visual_editor');
+add_filter('load-post-new.php', 'disable_visual_editor');
 
 // 投稿の自動保存を無効化
-function disable_autosave() {
-  wp_deregister_script('autosave');
-}
-// add_action('wp_print_scripts', 'disable_autosave');
+// add_filter('wp_print_scripts', function() {
+//   wp_deregister_script('autosave');
+// });
 
 // カテゴリーを無効化(非表示)
-function unregister_categories() {
-  register_taxonomy('category', array());
-}
-// add_action('init', 'unregister_categories');
+// add_filter('init', function() {
+//   register_taxonomy('category', array());
+// });
 // unregister_widget( 'WP_Widget_Categories' );
 
 // タグを無効化
-function unregister_tags() {
+add_filter('init', function() {
   unregister_taxonomy_for_object_type('post_tag', 'post');
-}
-add_action('init', 'unregister_tags');
+});
 
 // 投稿に画像を挿入するときのフォーマットを変更
-function my_remove_img_attr($html, $id, $alt, $title, $align, $size){
+function my_get_image_tag($html, $id, $alt, $title, $align, $size){
   $html = preg_replace('/ width="\d+"/', '', $html);
   $html = preg_replace('/ height="\d+"/', '', $html);
   $html = preg_replace('/ class=".+"/', '', $html);
   $html = preg_replace('/ title=".+"/', '', $html);
   return $html;
 }
-add_action('get_image_tag', 'my_remove_img_attr', 1 ,6);
+add_filter('get_image_tag', 'my_get_image_tag', 1 ,6);
 
 function my_image_send_to_editor( $html, $id, $caption, $title, $align, $url, $size ) {
   $html = preg_replace('/<a href=".+">/', '', $html);
@@ -117,42 +108,46 @@ function my_image_send_to_editor( $html, $id, $caption, $title, $align, $url, $s
 
   return $html;
 }
-add_action('image_send_to_editor', 'my_image_send_to_editor', 10 ,7);
+add_filter('image_send_to_editor', 'my_image_send_to_editor', 10 ,7);
 
 // 画像をアップロードしたときにファイル名をタイムスタンプに変更
-function rename_mediafile($filename) {
-	$info = pathinfo($filename);
+add_filter('sanitize_file_name', function($filename) {
+  $info = pathinfo($filename);
 	$ext  = empty($info['extension']) ? '' : '.' . $info['extension'];
 	if( $info['filename'] != 'sitemap' ){
 		$filename = strtolower(time().$ext);
 	}
   return $filename;
+}, 10);
+
+// Cloudinary
+// productionでだけ有効にする
+// developmentではAuto Cloudinaryの設定の「Content Images」のチェックを外す（アイキャッチが表示できなくなるため）
+if (getenv('SERVER_ENV') == 'production') {
+  // デフォルトのフォーマットを設定
+  add_filter('cloudinary_default_args', function($args) {
+    $args['transform']['crop'] = 'limit';
+  	$args['transform']['format'] = 'auto';
+  	$args['transform']['quality'] = 'auto:best';
+  	$args['transform']['flags'] = 'progressive';
+  	return $args;
+  });
+
+  // 本文中の画像URLをCloudinaryのものに置換する
+  add_filter('the_content', function($content) {
+    $pattern = '/<img.*?src=(["\'])(.+?)\1.*?>/i';
+    preg_match_all($pattern, $content, $matches);
+
+    foreach ($matches[2] as $url) {
+      $content = str_replace($url, cloudinary_url($url, array(
+        'transform' => array(
+          'width' => 1440
+        )
+      )), $content);
+    }
+
+    return $content;
+  });
 }
-add_filter('sanitize_file_name', 'rename_mediafile', 10);
-
-// Cloudinary settings
-add_filter('cloudinary_default_args', function($args) {
-  $args['transform']['crop'] = 'limit';
-	$args['transform']['format'] = 'auto';
-	$args['transform']['quality'] = 'auto:best';
-	$args['transform']['flags'] = 'progressive';
-	return $args;
-});
-
-add_shortcode('cloudinary', function($atts) {
-  extract(shortcode_atts(array(
-    'url' => '',
-  ), $atts));
-
-  return cloudinary_url($url, array(
-    'transform' => array(
-      'crop' => 'limit',
-      'width' => 1440,
-      'height' => 1440,
-      'format' => 'auto',
-      'quality' => 'auto:best'
-    )
-  ));
-});
 
 ?>
