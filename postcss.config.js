76dfa831489b5714b29c8c356cf5ba5cca38831e
Ryo Nakae <ryo.n.1129@gmail.com>
2018-01-26 15:22:45 +0900

    構成を最新版にアップデート

--- postcss.config.js
+++ postcss.config.js
@@ -6,27 +6,16 @@ const options = {
     path: path.join(filePath.assets, 'styles')
   },
   cssnext: {
-    browsers: [
-      'last 2 versions',
-      'ie > 11',
-      'iOS >= 10',
-      'Android >= 5.0'
-    ],
+    browsers: ['last 2 versions', 'IE >= 11', 'iOS >= 11', 'Android >= 5.0'],
     cascade: false
-  },
-  cssnano: {
-    preset: 'default',
-    autoprefixer: false,
-    discardUnused: {
-      fontFace: false
-    }
   }
 }
 
-module.exports = ({env}) => ({
+module.exports = ctx => ({
+  map: ctx.env === 'production' ? false : ctx.options.map,
   plugins: {
     'postcss-import': options.postcssImport,
     'postcss-cssnext': options.cssnext,
-    'cssnano': env === 'production' ? options.cssnano : false
+    csswring: ctx.env === 'production'
   }
 })
