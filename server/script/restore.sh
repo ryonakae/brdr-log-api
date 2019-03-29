440636f3a493eb4abb40a0d0fc0e72cc98816c0c
Ryo Nakae <ryo.n.1129@gmail.com>
2019-03-29 19:32:22 +0900

    update wp 5.1.1

--- server/script/restore.sh
+++ server/script/restore.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-EXEC='docker-compose exec -u www-data wordpress'
-
 # restore wp-content/uploads
 docker-compose run --rm data tar -xvf /backup/wp-content-uploads.tar
 
 # restore db by wp-cli
-eval $EXEC wp db import /backup/db.sql
+docker-compose run --rm wpcli db import /backup/db.sql
 
 # remove backup files
 docker-compose run --rm data rm -rf /backup/wp-content-uploads.tar /backup/db.sql
--- server/script/setup.sh
+++ server/script/setup.sh
@@ -1,35 +1,32 @@
 #!/bin/sh
 
-EXEC='docker-compose exec -u www-data wordpress'
-EXEC_ROOT='docker-compose exec -u root wordpress'
-
 # fix permission of wp-content
-eval $EXEC_ROOT chown -R www-data:www-data /var/www/html/wp-content
+docker-compose run --rm data chown -R www-data:www-data /var/www/html/wp-content
 
 # install & activate japanese lang file
-eval $EXEC wp core language install ja
-eval $EXEC wp site switch-language ja
+docker-compose run --rm wpcli core language install ja
+docker-compose run --rm wpcli core language activate ja
 
 # change permalink setting
-eval $EXEC wp rewrite structure '/post/%post_id%'
+docker-compose run --rm wpcli rewrite structure '/post/%post_id%'
 
 # activate my theme
-eval $EXEC wp theme activate log
+docker-compose run --rm wpcli theme activate log
 
 # delete default theme
-eval $EXEC wp theme delete \
+docker-compose run --rm wpcli theme delete \
   twentyfifteen \
   twentysixteen \
   twentyseventeen \
   twentynineteen
 
 # delete default plugin
-eval $EXEC wp plugin delete \
+docker-compose run --rm wpcli plugin delete \
   akismet \
   hello
 
 # install and actvate plugin
-eval $EXEC wp plugin install --activate \
+docker-compose run --rm wpcli plugin install --activate \
   update-control \
   wp-multibyte-patch \
   jetpack-markdown \
@@ -37,7 +34,7 @@ eval $EXEC wp plugin install --activate \
   force-regenerate-thumbnails
 
 # install Advanced Custom Fields & Repeater Field
-eval $EXEC wp plugin install \
+docker-compose run --rm wpcli plugin install \
   acf-to-rest-api \
   advanced-custom-fields \
   https://cl.ly/3S0C2G3T3w1z/download/acf-repeater.zip
