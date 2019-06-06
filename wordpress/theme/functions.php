e6eb39bad480d52e589413136b8b4f8be0a8fb40
Ryo Nakae <ryo.n.1129@gmail.com>
2019-06-06 18:13:48 +0900

    update

--- wordpress/theme/functions.php
+++ wordpress/theme/functions.php
@@ -78,6 +78,68 @@ add_filter('intermediate_image_sizes_advanced', 'delete_image_sizes');
 add_image_size('index_eyecatch', 1280, 1280, false);
 add_image_size('detail', 2560, 2560, false);
 
+// 投稿に画像をショートコードで挿入する
+function my_image_send_to_editor($html, $id, $caption, $title, $align, $url, $size, $alt) {
+  $image = wp_get_attachment_image_src($id, $size)[0];
+
+  if ($caption) {
+    if ($alt) {
+      $html = '[image src="' . $image . '" alt="' . $alt . '" caption="' . $caption . '"]' . "\n";
+    } else {
+      $html = '[image src="' . $image . '" caption="' . $caption . '"]' . "\n";
+    }
+  } else {
+    if ($alt) {
+      $html = '[image src="' . $image . '" alt="' . $alt . '"]' . "\n";
+    } else {
+      $html = '[image src="' . $image . '"]' . "\n";
+    }
+  }
+
+  return $html;
+}
+add_filter('image_send_to_editor', 'my_image_send_to_editor', 10 ,8);
+
+// 画像ショートコードを指定したフォーマットで展開する
+function shortcode_image($arg) {
+  extract(shortcode_atts(array (
+    'src' => '',
+    'alt' => '',
+    'caption' => ''
+  ), $arg));
+
+  if ($alt) {
+    $image = '<img src="' . $src . '" alt="' . $alt . '">';
+  } else {
+    $image = '<img src="' . $src . '">';
+  }
+
+  if ($caption) {
+    $html = '<figure class="img">' . "\n" .
+            '  ' . $image . "\n" .
+            '  ' . '<figcaption>' . $caption . '</figcaption>' . "\n" .
+            '</figure>';
+  } else {
+    $html = '<figure class="img">' . "\n" .
+            '  ' . $image . "\n" .
+            '</figure>';
+  }
+
+  return $html;
+}
+add_shortcode('image', 'shortcode_image');
+
+// 画像をアップロードしたときにファイル名をタイムスタンプに変更
+function rename_filename_to_timestamp($filename) {
+  $info = pathinfo($filename);
+	$ext  = empty($info['extension']) ? '' : '.' . $info['extension'];
+	if ($info['filename'] != 'sitemap') {
+		$filename = strtolower(time().$ext);
+	}
+  return $filename;
+}
+add_filter('sanitize_file_name', 'rename_filename_to_timestamp', 10);
+
 // WP APIに独自のフィールドを追加
 function register_rest_specific_fields() {
   // カテゴリーの詳細情報
