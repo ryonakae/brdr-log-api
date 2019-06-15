e557dd198d3226b66439681c92385aa15982dad6
Ryo Nakae <ryo.n.1129@gmail.com>
2019-06-15 14:46:03 +0900

    fix image sizes

--- wordpress/theme/functions.php
+++ wordpress/theme/functions.php
@@ -63,9 +63,6 @@ add_filter('jpeg_quality', 'change_image_quality');
 
 // 画像のサイズを削除
 function delete_image_sizes($sizes) {
-  // medium_large(Advanced Custom Fields)
-  update_option('medium_large', 0);
-
   // unset($sizes['thumbnail']);
   unset($sizes['medium']);
   unset($sizes['large']);
@@ -74,60 +71,7 @@ function delete_image_sizes($sizes) {
 add_filter('intermediate_image_sizes_advanced', 'delete_image_sizes');
 
 // 画像のサイズを追加
-// add_image_size('admin_thumbnail', 300, 300, true);
-add_image_size('index_eyecatch', 1280, 1280, false);
 add_image_size('detail', 2560, 2560, false);
-
-// 投稿に画像をショートコードで挿入する
-function my_image_send_to_editor($html, $id, $caption, $title, $align, $url, $size, $alt) {
-  $image = wp_get_attachment_image_src($id, $size)[0];
-
-  if ($caption) {
-    if ($alt) {
-      $html = '[image src="' . $image . '" alt="' . $alt . '" caption="' . $caption . '"]' . "\n";
-    } else {
-      $html = '[image src="' . $image . '" caption="' . $caption . '"]' . "\n";
-    }
-  } else {
-    if ($alt) {
-      $html = '[image src="' . $image . '" alt="' . $alt . '"]' . "\n";
-    } else {
-      $html = '[image src="' . $image . '"]' . "\n";
-    }
-  }
-
-  return $html;
-}
-add_filter('image_send_to_editor', 'my_image_send_to_editor', 10 ,8);
-
-// 画像ショートコードを指定したフォーマットで展開する
-function shortcode_image($arg) {
-  extract(shortcode_atts(array (
-    'src' => '',
-    'alt' => '',
-    'caption' => ''
-  ), $arg));
-
-  if ($alt) {
-    $image = '<img src="' . $src . '" alt="' . $alt . '">';
-  } else {
-    $image = '<img src="' . $src . '">';
-  }
-
-  if ($caption) {
-    $html = '<figure class="img">' . "\n" .
-            '  ' . $image . "\n" .
-            '  ' . '<figcaption>' . $caption . '</figcaption>' . "\n" .
-            '</figure>';
-  } else {
-    $html = '<figure class="img">' . "\n" .
-            '  ' . $image . "\n" .
-            '</figure>';
-  }
-
-  return $html;
-}
-add_shortcode('image', 'shortcode_image');
 
 // 画像をアップロードしたときにファイル名をタイムスタンプに変更
 function rename_filename_to_timestamp($filename) {
